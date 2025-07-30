52edaf84303226e7f9a4aeec7bfea58ee5a43742
George Raafat <georgexraafat@gmail.com>
2025-07-30 23:09:20 +0300

    add application properties and controllers for greeting and time services

--- springboot-fundamentals/src/main/java/ai/sprints/springboot_fundamentals/time/TimeController.java
+++ springboot-fundamentals/src/main/java/ai/sprints/springboot_fundamentals/time/TimeController.java
@@ -10,7 +10,7 @@ public class TimeController {
     private final TimeService timeService;
 
     @Autowired
-    public TimeController(@Qualifier("mock") TimeService timeService) {
+    public TimeController(TimeService timeService) {
         this.timeService = timeService;
     }
 
